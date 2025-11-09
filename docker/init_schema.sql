e65770256f7d3bf028c64d7def49ec8c3fc134eb
Hwangseoeun <sunny030921@gmail.com>
2025-11-09 17:56:00 +0900

    chore: lotto_statistic 테이블 생성 쿼리 추가

--- docker/init_schema.sql
+++ docker/init_schema.sql
@@ -3,3 +3,16 @@ CREATE TABLE member (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(30) NOT NULL UNIQUE
 );
+
+/* lotto_statistic 테이블 생성 쿼리 */
+CREATE TABLE lotto_statistic (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    purchase_price INT NOT NULL,
+    return_rate FLOAT NOT NULL,
+    member_id BIGINT NOT NULL,
+    CONSTRAINT fk_lotto_statistic_member
+        FOREIGN KEY (member_id)
+        REFERENCES member (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);

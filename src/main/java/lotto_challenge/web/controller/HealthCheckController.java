ed227e8165ef6b7f0fa7660ba29f35f33aac8b81
Hwangseoeun <sunny030921@gmail.com>
2025-11-14 02:00:01 +0900

    refactor: 패키지 이동

--- src/main/java/lotto_challenge/web/controller/HealthCheckController.java
+++ src/main/java/lotto_challenge/web/controller/HealthCheckController.java
@@ -1,4 +1,4 @@
-package lotto_challenge.web.common;
+package lotto_challenge.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;

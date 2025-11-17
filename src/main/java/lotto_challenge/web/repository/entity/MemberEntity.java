5e97a9fd5921270ab428c5b57eaf2b6432da2737
Hwangseoeun <sunny030921@gmail.com>
2025-11-17 19:05:29 +0900

    feat: jpa 추가에 따른 entity 추가

---
+++ src/main/java/lotto_challenge/web/repository/entity/MemberEntity.java
@@ -0,0 +1,31 @@
+package lotto_challenge.web.repository.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lotto_challenge.core.model.Member;
+
+@Table(name = "member")
+@Entity
+public class MemberEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30, nullable = false, unique = true)
+    private String email;
+
+    public MemberEntity() {}
+
+    public MemberEntity(final Member member) {
+        this.email = member.getEmail();
+    }
+
+    public Member toMember() {
+        return new Member(email);
+    }
+}

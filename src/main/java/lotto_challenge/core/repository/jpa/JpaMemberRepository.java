36e15b5164326fed47ebcb91424c88c29ab58f9f
Hwangseoeun <sunny030921@gmail.com>
2025-11-18 18:58:38 +0900

    refactor: JPQL 쿼리문 추가

--- src/main/java/lotto_challenge/core/repository/jpa/JpaMemberRepository.java
+++ src/main/java/lotto_challenge/core/repository/jpa/JpaMemberRepository.java
@@ -2,9 +2,16 @@ package lotto_challenge.core.repository.jpa;
 
 import lotto_challenge.core.repository.jpa.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface JpaMemberRepository extends JpaRepository<MemberEntity, Long> {
 
     boolean existsByEmail(final String email);
-    Long findMemberIdByEmail(final String email);
+
+    @Query(
+        "SELECT m.id " +
+        "FROM MemberEntity m " +
+        "WHERE m.email = :email"
+    )
+    Long findIdByEmail(final String email);
 }

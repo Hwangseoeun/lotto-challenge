7f503729882fc34eb5633f18afb1189d93d308ff
Hwangseoeun <sunny030921@gmail.com>
2025-11-19 10:46:02 +0900

    feat: api 통신 실패 시 안내 문구 출력 로직 추가

--- src/main/java/lotto_challenge/console/view/LottoOutputGuideMessage.java
+++ src/main/java/lotto_challenge/console/view/LottoOutputGuideMessage.java
@@ -2,6 +2,7 @@ package lotto_challenge.console.view;
 
 public enum LottoOutputGuideMessage {
 
+    OUTPUT_SERVER_EXCEPTION_GUIDE_MESSAGE("서버와 통신할 수 없습니다."),
     OUTPUT_LOTTO_QUANTITY_GUIDE_MESSAGE("개를 구매했습니다."),
     OUTPUT_WINNING_RESULT_GUIDE_MESSAGE("당첨 통계\n---"),
     OUTPUT_WINNING_RESULT_SECOND_PRIZE_GUIDE_MESSAGE("%d개 일치, 보너스 볼 일치 (%,d원) - %d개"),
